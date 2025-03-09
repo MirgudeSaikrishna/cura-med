7821b04f78ab93ba7c79e9a93e99868ffaa516dc
MirgudeSaikrishna <saimirgude@gmail.com>
2025-03-09 16:54:41 +0530

    added pagination and search for products

--- src/pages/Uproduct.jsx
+++ src/pages/Uproduct.jsx
@@ -2,6 +2,10 @@ import React,{useEffect,useState} from 'react';
 const Uproduct=()=> {
     const [products,setProducts]=useState([]);
     const shopName=localStorage.getItem('shopName');
+    const [currentPage,setCurrentPage]=useState(1);
+    const [currentProducts,setCurrentProducts]=useState([]);
+    const [productsPerPage]=useState(3);
+    const [searchdata,setSearchdata]=useState('');
     useEffect(()=>{
         async function getProducts(){
             const response=await fetch('http://localhost:1337/api/products',{
@@ -23,14 +27,34 @@ const Uproduct=()=> {
         }
         getProducts()
     },[shopName])
+    function searchProducts(searchdata){
+        searchdata=searchdata.toLowerCase();
+        return function(product){
+            return product.name.toLowerCase().includes(searchdata)||!searchdata;
+        }
+    }
+    useEffect(()=>{
+        const filteredProducts = products.filter(searchProducts(searchdata));
+        const lastPostIndex=currentPage*productsPerPage;
+        const firstPostIndex=lastPostIndex-productsPerPage;
+        setCurrentProducts(filteredProducts.slice(firstPostIndex,lastPostIndex));
+    },[currentPage,products, searchdata])
+    const pageNumbers=[];
+    for(let i=1;i<=Math.ceil(products.length/productsPerPage);i++){
+        pageNumbers.push(i);
+    }
     return (
         <div>
             <h1>Products</h1>
+            <input type="text" value={searchdata} onChange={(e)=>setSearchdata(e.target.value)} placeholder="Search"/>
             <ul>
-                {products.map((product,index)=>(
+                {currentProducts.map((product,index)=>(
                     <li key={product._id}>{product.name} {product.price} {product.description}</li>
                 ))}
             </ul>
+            <div>{ pageNumbers.map((number)=>(
+                <button key={number} onClick={()=>setCurrentPage(number)}>{number}</button>))
+            }</div>
             <input type="button" onClick={()=>{localStorage.removeItem('shopName');window.location.href='/User_view'}} value="Back"/>
         </div>
     );
